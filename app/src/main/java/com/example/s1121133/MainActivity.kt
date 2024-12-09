898405475e42c056a91c6101b663ff4fd19f2f65
student <melisaqq55@gmail.com>
2024-12-09 15:35:47 +0800

    v2_許閔慈

--- app/src/main/java/com/example/s1121133/MainActivity.kt
+++ app/src/main/java/com/example/s1121133/MainActivity.kt
@@ -5,14 +5,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.s1121133.ui.theme.S1121133Theme
+import androidx.compose.ui.platform.LocalContext
+import android.app.Activity
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,13 +44,51 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+
+
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    Column (Modifier.background(Color(0xff95fe95))){
+        Alignment.CenterHorizontally
+            Text(
+                text = "2024期末上機考(資管二A許閔慈)",
+                modifier = modifier
+            )
+            Image(
+                painter = painterResource(id = R.drawable.class_a),
+                contentDescription = "圖片",
+            )
+        Text(
+            text = "2024期末上機考(資管二A許閔慈)",
+            modifier = modifier
+        )
+
+        Text(
+            text = "您的成績 0 分",
+            modifier = modifier
+        )
+
+        val activity = (LocalContext.current as? Activity)
+        Box (
+            contentAlignment = Alignment.BottomEnd
+        ){
+            Button(
+                onClick = {
+                    activity?.finish()
+                }
+            ) {
+                Text("結束App")
+            }
+        }
+
+
+    }
+
+    }
+
+
 
 @Preview(showBackground = true)
 @Composable
